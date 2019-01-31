b569124bacb54fbaa41489c1611f2e553784d7ef
Jizhou Li <hijizhou@gmail.com>
2019-01-30 23:21:59 -0800

    finish SSRL data reading

--- dxchange/lijz_dataio.py
+++ dxchange/lijz_dataio.py
@@ -1,9 +1,10 @@
 
-import tomopy as tp
+# import tomopy as tp
 import numpy as np
 import matplotlib.pyplot as plt
-from mayavi import mlab
-import dxchange
+# from mayavi import mlab
+from dxchange import exchange
+# import dxchange.reader as dr
 
 # obj = tp.lena()
 # plt.imshow(obj[0,:,:])
@@ -15,17 +16,32 @@ import dxchange
 # mlab.contour3d(obj[:,:,:])
 # mlab.show()
 
-h5fname = '/media/hijizhou/Data2/NeronXRecon/Data/brain2_MOSAIC-TOMO_180722_2039/rep01_00091_brain2_-085.00_Degree_x01_y00_00000.00_eV_001of001.xrm'
+# h5fname = '/media/hijizhou/Data2/NeronXRecon/Data/brain2_MOSAIC-TOMO_180722_2039/rep01_00091_brain2_-085.00_Degree_x01_y00_00000.00_eV_001of001.xrm'
 
-proj, metadata = dxchange.reader.read_xrm(h5fname, [])
+h5fname3 = '/Users/hijizhou/Downloads/rep01_00038_brain2_-088.00_Degree_x02_y00_00000.00_eV_001of001.xrm'
 
-h5fname2 = '/media/hijizhou/Data2/NeronXRecon/Data/brain2_MOSAIC-TOMO_180722_2039/rep01_02996_brain2_0076.00_Degree_x08_y00_00000.00_eV_001of001.xrm'
+# proj, metadata = dxchange.reader.read_xrm(h5fname, [])
+#
+# h5fname2 = '/media/hijizhou/Data2/NeronXRecon/Data/brain2_MOSAIC-TOMO_180722_2039/rep01_02996_brain2_0076.00_Degree_x08_y00_00000.00_eV_001of001.xrm'
+#
+# proj, metadata2 = dxchange.reader.read_xrm(h5fname2, [])
 
-proj, metadata2 = dxchange.reader.read_xrm(h5fname2, [])
+
+# h5fname3 = '/media/hijizhou/Data2/NeronXRecon/Data/brain2_MOSAIC-TOMO_180722_2039/'
 
 
-import exchange as ec
-ec.app_ssrl_xrm_mosaic_metadata('rep01_02996_brain2_0076.00_Degree_x08_y00_00000.00_eV_001of001.xrm')
+h5fname3 = '/Users/hijizhou/Downloads/TestData/'
+
+
+tomo, flat, metadata = exchange.read_slac_ssrl(h5fname3, 0)
+#
+# arr, metadata = exchange.dxreader.read_xrm_stack(h5fname3 + 'rep01_00038_brain2_-088.00_Degree_x02_y00_00000.00_eV_001of001.xrm.xrm', list(range(0, 1)))
+
+
+
+
+# import exchange as ec
+# ec.app_ssrl_xrm_mosaic_metadata('rep01_02996_brain2_0076.00_Degree_x08_y00_00000.00_eV_001of001.xrm')
 
 
 h5fname = '/media/hijizhou/Data2/NeronXRecon/Data/201807221943_8000_eV_ref.xrm'
